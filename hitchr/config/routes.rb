70c1f8b8087606af7cce2a760e38ca65ff0ee0f9
Hira Kazmi <hirarukh@gmail.com>
2016-07-18 23:53:36 -0400

    set up routes

--- hitchr/config/routes.rb
+++ hitchr/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-
+  resources :rides
+  resources :reviews
+  resources :bookings
 end
