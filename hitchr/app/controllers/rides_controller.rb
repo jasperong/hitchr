4739a7bf709d17e330586e744bc593fad6194b1b
Hira Kazmi <hirarukh@gmail.com>
2016-07-18 23:52:38 -0400

    set up ride controller actions

--- hitchr/app/controllers/rides_controller.rb
+++ hitchr/app/controllers/rides_controller.rb
@@ -1,2 +1,31 @@
 class RidesController < ApplicationController
+
+  def index
+    @rides = Ride.all
+  end
+
+  def show
+    @ride = Ride.find(params[:id])
+  end
+
+  def new
+    @ride = Ride.new
+  end
+
+  def create
+    @ride = Ride.find(ride_params)
+
+    if @ride.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @ride = Ride.find(params[:id])
+    @ride.destroy
+    redirect_to root_path
+  end
+
 end
