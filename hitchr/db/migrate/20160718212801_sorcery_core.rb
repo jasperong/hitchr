deb09c2f4ba7084c4e816f6d63f58908d780493d
Hira Kazmi <hirarukh@gmail.com>
2016-07-18 22:08:33 -0400

    created rails app, user model, attatched paperclip gem

---
+++ hitchr/db/migrate/20160718212801_sorcery_core.rb
@@ -0,0 +1,18 @@
+class SorceryCore < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email,            :null => false
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.date :date_of_birth
+      t.float :reliability
+      t.string :crypted_password
+      t.string :salt
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
