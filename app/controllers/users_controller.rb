aec09dffbea9a931f5059ebb509ef4bfb2d8fe2c
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-20 15:34:01 -0400

    The User Profile Edit Page is Working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,43 @@
 class UsersController < ApplicationController
-  # before_action :require_login, only: :show
+	# before_action :require_login, only: :show
 
 
-  def new
-    @user = User.new
-  end
+	def new
+		@user = User.new
+	end
 
-  def show
-    @user = User.find(params[:id])
-  end
+	def show
+		@user = User.find(params[:id])
+	end
 
-  def create
-    @user = User.new(user_params)
+	def create
+		@user = User.new(user_params)
 
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path, alert: "Signed up!"
-    else
-      render :new
-    end
-  end
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to root_path, alert: "Signed up!"
+		else
+			render :new
+		end
+	end
 
-  def edit
-  end
+	def edit
+		@user = User.find(params[:id])
+	end
 
-  def update
-    if @user.update_attributes(user_params)
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			redirect_to user_path(@user)
+		else
+			render :new
+		end
+	end
 
-  private
+	private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :gender, :date_of_birth, :reliability, :avatar)
-  end
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :gender, :date_of_birth, :reliability, :avatar)
+	end
 
 end
