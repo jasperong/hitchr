b82283e22a1ab25b15fc4b73a849189b8c1ea40e
Althea Yi <altheaserena.yi@gmail.com>
2016-07-20 19:59:54 -0400

    Added and passed factorygirl test for password presence

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,9 +8,13 @@ class UserTest < ActiveSupport::TestCase
 
   test "should be invalid without an email" do
     @user.email = nil
-
     assert_nil @user.email # AY: added presence validation in model
+    assert_equal false, @user.valid?
+  end
 
+  test "should be invalid without a password" do
+    @user.password = nil
+    assert_nil @user.password
     assert_equal false, @user.valid?
   end
 
