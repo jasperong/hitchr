aa803df4c40266e7b34c2fa795e1d63c087f7148
Hira Kazmi <hirarukh@gmail.com>
2016-07-21 17:33:12 -0400

    changed routes and associations for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-	# root "rides#index"
-	resources :sessions, only: [:new, :create, :destroy]
-	resources :reviews
-	resources :bookings
+  # root "rides#index"
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :bookings
 
-	resources :users do
-		resources :bookings, only: [:show]
-		resources :reviews, only: [:show]
-	end
+  resources :users do
+    resources :bookings, only: [:show]
+    resources :reviews, only: [:show, :new, :create]
+  end
 
-	resources :rides do
-		resources :bookings
-	end
+  resources :rides do
+    resources :bookings
+  end
 
 end
