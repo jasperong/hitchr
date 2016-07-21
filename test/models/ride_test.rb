b836c5274a692d0eaf28115bb32376b9322500d7
David Cisneros <d.cisneros849@gmail.com>
2016-07-21 18:08:10 -0400

    Created ride test for future

--- test/models/ride_test.rb
+++ test/models/ride_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class RideTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+
+    @ride = build(:ride)
+  end
+
+  test "should only be valid if ride date is in future" do
+    @ride.time > Time.now
+    assert_equal true, @ride.valid?
+  end
 end
