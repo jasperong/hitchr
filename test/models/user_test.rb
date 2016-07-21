a6a557c7f1aabd6a7f4a236a5844cd9fd2be2096
Althea Yi <altheaserena.yi@gmail.com>
2016-07-20 20:11:47 -0400

    Created factorygirl test for birthday presence, passed.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -42,5 +42,11 @@ class UserTest < ActiveSupport::TestCase
     assert_equal false, @user.valid?
   end
 
+  test "should be invalid without birthday" do
+    @user.date_of_birth = nil
+    assert_nil @user.date_of_birth
+    assert_equal false, @user.valid?
+  end
+
 
 end
