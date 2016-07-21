db507767213f54c5885d4100cd3f6ebdf6dc038a
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-21 14:46:36 -0400

    Removed the root route for rides

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  root "rides#index"
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :reviews
-  resources :bookings
+	# root "rides#index"
+	resources :sessions, only: [:new, :create, :destroy]
+	resources :reviews
+	resources :bookings
 
-  resources :users do
-    resources :bookings, only: [:show]
-    resources :reviews, only: [:show]
-  end
+	resources :users do
+		resources :bookings, only: [:show]
+		resources :reviews, only: [:show]
+	end
 
-  resources :rides do
-    resources :bookings
-  end
+	resources :rides do
+		resources :bookings
+	end
 
 end
