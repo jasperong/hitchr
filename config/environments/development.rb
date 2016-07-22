69cec586c6acb576011afce389a19dfb26859124
Althea Yi <altheaserena.yi@gmail.com>
2016-07-22 15:52:13 -0400

    Added letter_opener gem properly and modified enviro dev file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
+  # Add this line to use letter_opener gem
+  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
