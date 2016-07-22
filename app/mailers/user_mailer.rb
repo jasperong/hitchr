e81c6be1239e721c6bc488684f8240dbeab812b1
Althea Yi <altheaserena.yi@gmail.com>
2016-07-22 15:07:22 -0400

    Added figaro stuff

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,5 +2,15 @@ class UserMailer < ApplicationMailer
 
   # 1. Setup a welcome email for new users
   def welcome
-  end 
+    @user = 
+    mail(to: @user.email, subject: "Welcome to Hitchr!")
+  end
 end
+
+  # 2. Setup an email for the driver to see when someone requests a seat
+
+  # 3. Setup an email for a rider when a driver accepts OR rejects a seat request
+
+  # 4. Setup an email when a booked seat is cancelled by a rider
+
+  # 5. Setup an email when a ride is cancelled by a driver
