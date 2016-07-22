8201ff7b15965636db1453443a795f8333a097e0
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-22 18:40:29 -0400

    Fixed booking cancel controller for the third time

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,7 +16,7 @@ class BookingsController < ApplicationController
 	end
 
 	def destroy
-		@booking = @ride.bookings.find(params[:id])
+		@booking = Booking.find(params[:id])
 		@booking.destroy
 		UserMailer.cancelled_seat(@ride).deliver_later
 		redirect_to user_path(@booking.user)
