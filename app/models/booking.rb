dc846c85fb43222bff10b89838da0d67bde478f2
David Cisneros <d.cisneros849@gmail.com>
2016-07-22 18:33:24 -0400

    users cant make reviews to themselves anymore

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,18 +1,4 @@
 class Booking < ActiveRecord::Base
   belongs_to :ride
   belongs_to :user
-  # 
-  # validate :enough_seats
-  #
-  # def enough_seats
-  #   @ride = Ride.find(params[:user_id])
-  #
-  #   arr = []
-  #   @ride.bookings.all.each { |booking| arr << booking.seats }
-  #   total_seats = arr.inject(:+)
-  #   if total_seats > @ride.seats_available
-  #     errors.add("Sorry, there are no more seats available.")
-  #   end
-  # end
-
 end
