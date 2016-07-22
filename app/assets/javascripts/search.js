efdf0acafdb4e6486c1849180495296b70a61905
Jasper <jave.ong4@yahoo.com>
2016-07-21 22:49:14 -0400

    Search function working, further testing needed

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,19 +1,4 @@
 $(function() {
-  // $('.search').autocomplete({
-  //   source: $.ajax({
-  //     url: "https://maps.googleapis.com/maps/api/place/autocomplete/",
-  //     method: "get",
-  //     data: {
-  //       input: "json",
-  //       key: apiKey,
-  //       country: "CA",
-  //       type: "locality"
-  //       },
-  //     dataType: "json"
-  //   }).done(function(responseData) {
-  //     console.log(responseData);
-  //   });
-  // });
   $('#start_location').geocomplete({
     country: "CA",
     types: ["(cities)"]
@@ -22,6 +7,4 @@ $(function() {
     country: "CA",
     types: ["(cities)"]
   });
-  });
-
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root "home#index"
+  get "/search" => "home#search"
   resources :sessions, only: [:new, :create, :destroy]
   resources :reviews
   resources :bookings
