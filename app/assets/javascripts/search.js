774def1ecb870e9f41aa255b664ffc7280676aa5
Jasper <jave.ong4@yahoo.com>
2016-07-22 15:53:49 -0400

    commit before merge

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -7,4 +7,20 @@ $(function() {
     country: "CA",
     types: ["(cities)"]
   });
+
+  $('.search-button').on('click', function(e) {
+    e.preventDefault();  
+
+    $.ajax({
+      url: $(this).attr('action'),
+      method: 'get',
+      data: {
+        start_location: $('#start_location').val(),
+        end_location: $('#end_location').val()
+      },
+      dataType: 'html'
+    }).done(function(responseData) {
+      console.log(responseData);
+    });
+  });
 });
