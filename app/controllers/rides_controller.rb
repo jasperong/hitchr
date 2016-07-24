8e113b11ad0b62abcc5a0effcff41781702f420c
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-24 15:22:38 -0400

    Got the Cancel ride working

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,38 +1,38 @@
 class RidesController < ApplicationController
 
-  def index
-    @rides = Ride.all
-  end
+	def index
+		@rides = Ride.all
+	end
 
-  def show
-    @ride = Ride.find(params[:id])
-    @booking = @ride.bookings.build
-  end
+	def show
+		@ride = Ride.find(params[:id])
+		@booking = @ride.bookings.build
+	end
 
-  def new
-    @ride = Ride.new
-  end
+	def new
+		@ride = Ride.new
+	end
 
-  def create
-    @ride = Ride.new(ride_params)
-    @ride.user_id = current_user.id
-    if @ride.save
-      redirect_to user_path(current_user)
-    else
-      render :new
-    end
-  end
+	def create
+		@ride = Ride.new(ride_params)
+		@ride.user_id = current_user.id
+		if @ride.save
+			redirect_to user_path(current_user)
+		else
+			render :new
+		end
+	end
 
-  def destroy
-    @ride = Ride.find(params[:id])
-    @ride.destroy
-    UserMailer.cancelled_ride(@ride).deliver_later 
-    redirect_to root_path
-  end
+	def destroy
+		@ride = Ride.find(params[:id])
+		@ride.destroy
+		# UserMailer.cancelled_ride(@ride).deliver_later 
+		redirect_to root_path
+	end
 
-  private
-  def ride_params
-    params.require(:ride).permit(:date, :time, :seats_available, :comments, :start_location, :end_location)
-  end
+	private
+	def ride_params
+		params.require(:ride).permit(:date, :time, :seats_available, :comments, :start_location, :end_location)
+	end
 
 end
