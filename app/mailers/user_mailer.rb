8cdcb55d4cb5ac1b5f63e4caa5d878dddb6c054f
Althea Yi <altheaserena.yi@gmail.com>
2016-07-24 14:03:24 -0400

    Got user mailer for seat confirmation request working

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,12 +11,15 @@ class UserMailer < ApplicationMailer
     @ride = ride
     email = @ride.user.email
     mail(to:email, subject: "Somebody wants to hitch a ride with you on #{@ride.date}
-                            from #{@ride.start_location} #{@ride.end_location}")
+                            from #{@ride.start_location} to #{@ride.end_location}")
   end
 
 
-  # 3. Setup an email for a rider when a driver accepts OR rejects a seat request
-  def 
+  # 3. Setup an email for a rider when a driver rejects a seat request
+  def seat_confirmed(ride)
+    @ride = ride
+    email = booking.user.email
+  end
 
   # 4. Setup an email when a booked seat is cancelled by a rider
 
