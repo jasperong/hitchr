694c0a0bab22c96f3e91d332c6a9ff3b350f58b2
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-25 13:15:49 -0400

    Added the default image

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true # AY: Added presence validation
 
   # ========> for paperclip <=======
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "250x250>", thumb: "100x100>" }, default_url: "circle_arrow2.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   private
