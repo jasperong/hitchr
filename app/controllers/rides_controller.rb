12ed8ff4622c0689b6dce0a5fd4ad61fcfbbe471
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-26 13:37:13 -0400

    Added Price to the rides schema, make sure to rake db migrate!

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -24,7 +24,7 @@ class RidesController < ApplicationController
 
   def destroy
     @ride = Ride.find(params[:id])
-    
+
     UserMailer.cancelled_ride(@ride).deliver_later
     @ride.destroy
     redirect_to user_path(current_user)
@@ -32,7 +32,7 @@ class RidesController < ApplicationController
 
   private
   def ride_params
-    params.require(:ride).permit(:date, :time, :seats_available, :comments, :start_location, :end_location)
+    params.require(:ride).permit(:date, :time, :seats_available, :price, :comments, :start_location, :end_location)
   end
 
 end
