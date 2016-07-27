709b6de016203c77895523f5c69b972d8843e5b0
Jasper <jave.ong4@yahoo.com>
2016-07-27 17:22:32 -0400

    Ajax search working now

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -9,14 +9,14 @@ $(function() {
   });
 
   $('.search').on('submit', function(e) {
-    // e.preventDefault();
+    e.preventDefault();
     $.ajax({
       url: $(this).attr('action'),
       method: 'get',
       data: {
         start_location: $('#start_location').val(),
         end_location: $('#end_location').val(),
-        date: $('#date_')[0].val()
+        date: $('#date_').val()
         },
       dataType: 'html'
     }).done(function(responseData) {
