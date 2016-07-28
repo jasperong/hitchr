31e86e0ed05fd11b1c59ea5b7750411f2f45c84e
Hira Kazmi <hirarukh@gmail.com>
2016-07-28 11:24:20 -0400

    fixing @ride bug

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,20 +1,12 @@
 $(document).ready(function() {
 
-    // $("html").niceScroll({
-    //     cursorcolor:"rgba(30,30,30,.5)",
-    //     zindex:999,
-    //     scrollspeed: 1000,
-    //     mousescrollstep:100,
-    //     cursorborder:"0px solid #fff",
-    // });
-
-
+// For parallax on homepage
     $('.parallax-window').parallax({
     naturalWidth: 600,
     naturalHeight: 400
     });
 
-
+// For background Video on homepage
     $('#my-video').backgroundVideo({
       $videoWrap: $('#video-wrap'),
       $outerWrap: $('#outer-wrap'),
@@ -22,17 +14,32 @@ $(document).ready(function() {
       pauseVideoOnViewLoss: true,
       parallaxOptions: {
           offset: 60,
-          effect: 1.8
+          effect: 3
       }
     });
 
+
+// Change background of nav on scroll
+    $(window).scroll(function() { // check if scroll event happened
+         if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+           $(".navbar-fixed-top").css("background-color", "rgba(0, 0, 0, 0.8)"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+         } else {
+           $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+         }
+       });
+
+    $('.navbar-toggle').on('click', function(){
+
+      $(".navbar-fixed-top").css("background-color", "rgba(0, 0, 0, 0.8)");
+
+    });
+
+
+// For search button, on click scroll down to div containing searches
     $('.search-button').on('click', function (){
-
       $('.placeholder-info').hide();
-
       $('html, body').animate({
         scrollTop: $('#content_2').offset().top }, 1000);
-
       });
 
 
