2d17acfae1416daf93b34ed99c429b59fe71205b
Hira Kazmi <hirarukh@gmail.com>
2016-07-28 19:08:00 -0400

    ride show styles

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 // Change background of nav on scroll
     $(window).scroll(function() { // check if scroll event happened
          if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-           $(".navbar-fixed-top").css("background-color", "rgba(0, 0, 0, 0.8)"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+           $(".navbar-fixed-top").css("background-color", "rgba(0, 0, 0, 0.4)"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
          } else {
            $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
          }
@@ -33,7 +33,7 @@ $(document).ready(function() {
       $(".navbar-fixed-top").toggleClass('alt-color');
     });
 
-  
+
 
 
 // For search button, on click scroll down to div containing searches
