52ca7bfaab6ab565cb790ebfe53e362cb752e80d
Jasper <jave.ong4@yahoo.com>
2016-08-08 19:15:13 -0400

    Email commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'https://salty-scrubland-98291.herokuapp.com/' }
+
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
