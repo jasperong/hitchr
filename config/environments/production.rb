8d42f959093d6dfe1b9e13013ba5e800633f77f4
Jasper <jave.ong4@yahoo.com>
2016-08-08 20:11:04 -0400

    Emails send on development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://salty-scrubland-98291.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'salty-scrubland-98291.herokuapp.com/' }
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
